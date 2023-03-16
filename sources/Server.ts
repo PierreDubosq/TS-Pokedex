f48f05d95547757940e41f934daf6f3979e9001a
Pierre Dubosq <pierre@dubosq.eu>
2023-03-16 13:50:34 -0600

    Feat: add Server constructor

--- sources/Server.ts
+++ sources/Server.ts
@@ -1,4 +1,27 @@
+import { ApolloServer } from '@apollo/server';
+import express, { Application } from 'express';
+
+
 class Server {
+  private server: ApolloServer;
+  private app: Application;
+
+  constructor() {
+    this.server = new ApolloServer({
+      typeDefs: `
+        type Query {
+          hello: String
+        }
+      `,
+      resolvers: {
+        Query: {
+          hello: () => 'Hello world!',
+        },
+      },
+    });
+
+    this.app = express();
+  }
 }
 
 
