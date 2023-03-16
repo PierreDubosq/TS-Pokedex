deb6f63edb96e0ffdec163a2f26d31f8d2775bb0
Pierre Dubosq <pierre@dubosq.eu>
2023-03-16 14:26:32 -0600

    Feat: add MONGO_INITDB_ROOT_PASSWORD Config method

--- api/utils/Config.ts
+++ api/utils/Config.ts
@@ -10,6 +10,15 @@ class Config {
   public static get MONGO_INITDB_ROOT_USERNAME(): string {
     return env.get('MONGO_INITDB_ROOT_USERNAME').required().asString();
   }
+
+  /**
+   * @brief Get the MONGO_INITDB_ROOT_PASSWORD variable from the environment
+   * 
+   * @returns The MONGO_INITDB_ROOT_PASSWORD variable
+   */
+  public static get MONGO_INITDB_ROOT_PASSWORD(): string {
+    return env.get('MONGO_INITDB_ROOT_PASSWORD').required().asString();
+  }
 }
 
 
