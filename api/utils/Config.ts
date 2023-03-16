b0600b6bbc5b32134370ff7be6d0f21713097450
Pierre Dubosq <pierre@dubosq.eu>
2023-03-16 14:26:23 -0600

    Feat: add MONGO_INITDB_ROOT_USERNAME Config method

--- api/utils/Config.ts
+++ api/utils/Config.ts
@@ -1,4 +1,15 @@
+import env from 'env-var';
+
+
 class Config {
+  /**
+   * @brief Get the MONGO_INITDB_ROOT_USERNAME variable from the environment
+   * 
+   * @returns The MONGO_INITDB_ROOT_USERNAME variable
+   */
+  public static get MONGO_INITDB_ROOT_USERNAME(): string {
+    return env.get('MONGO_INITDB_ROOT_USERNAME').required().asString();
+  }
 }
 
 
