a649c292e029b972302440be842ee1a29b9b5226
Pierre Dubosq <pierre@dubosq.eu>
2023-03-16 21:52:45 -0600

    Feat: add Database connect method

--- api/sources/Database.ts
+++ api/sources/Database.ts
@@ -1,4 +1,31 @@
+import Config from '../utils/Config';
+
+import mongoose from 'mongoose';
+import Logger from 'poseidon-logger';
+
+
 class Database {
+  /**
+   * @brief Connect to the database
+   * 
+   * @param retry Number of retries
+   */
+  public static async connect(retry = 10): Promise<void> {
+    const uri: string = `mongodb://${Config.MONGO_INITDB_ROOT_USERNAME}:${Config.MONGO_INITDB_ROOT_PASSWORD}@database:27017/${Config.MONGO_INITDB_DATABASE}?authSource=admin`;
+
+    try {
+      await mongoose.connect(uri);
+      Logger.info('Connected to database');
+    } catch (error: unknown) {
+      if (retry > 0) {
+        Logger.warn(`Failed to connect to database, retrying in 5 seconds (${retry} retries left)`);
+        await new Promise<void>((resolve: () => void): NodeJS.Timeout => setTimeout(resolve, 5000));
+        await Database.connect(retry - 1);
+      } else {
+        throw error;
+      }
+    }
+  }
 }
 
 
