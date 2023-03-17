ec29a5af9e2ce5471319908e066397bce7f29c0b
Pierre Dubosq <pierre@dubosq.eu>
2023-03-16 23:07:46 -0600

    Feat: add JsonWebToken sign method

--- api/utils/JsonWebToken.ts
+++ api/utils/JsonWebToken.ts
@@ -1,4 +1,20 @@
+import env from 'env-var';
+import jwt from 'jsonwebtoken';
+
+
 class JsonWebToken {
+  /**
+   * @brief Sign a payload with a secret key
+   * 
+   * @param payload Payload to sign
+   * 
+   * @returns Signed token
+   */
+  public static sign = (payload: string | object | Buffer): string => {
+    const secret: string = env.get('TOKEN_KEY').required().asString();
+    const token: string = jwt.sign(payload, secret, { expiresIn: '1h' });
+    return token;
+  }
 }
 
 
