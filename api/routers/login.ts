60051fb2e8599cc27a6ba2096381d6d7e8c7ddbb
Pierre Dubosq <pierre@dubosq.eu>
2023-03-20 17:05:39 -0600

    Refacto: restore internal server error

--- api/routers/login.ts
+++ api/routers/login.ts
@@ -15,78 +15,87 @@ router.post('/login',
     request: Request,
     response: Response
   ): Promise<Response> => {
-    const {
-      username,
-      password
-    }: {
-      username: string | undefined,
-      password: string | undefined
-    } = request.body;
+    try {
+      const {
+        username,
+        password
+      }: {
+        username: string | undefined,
+        password: string | undefined
+      } = request.body;
 
-    Logger.debug(`POST /login: ${JSON.stringify(request.body)}`);
+      if (!username) {
+        Logger.warn('Error in POST /login: Username is required');
 
-    if (!username) {
-      Logger.warn('Error in POST /login: Username is required');
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            message: ReasonPhrases.BAD_REQUEST,
+            error: 'Username is required'
+          });
+      }
 
-      return response
-        .status(StatusCodes.BAD_REQUEST)
-        .json({
-          message: ReasonPhrases.BAD_REQUEST,
-          error: 'Username is required'
-        });
-    }
+      if (!password) {
+        Logger.warn('Error in POST /login: Password is required');
 
-    if (!password) {
-      Logger.warn('Error in POST /login: Password is required');
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            message: ReasonPhrases.BAD_REQUEST,
+            error: 'Password is required'
+          });
+      }
 
-      return response
-        .status(StatusCodes.BAD_REQUEST)
-        .json({
-          message: ReasonPhrases.BAD_REQUEST,
-          error: 'Password is required'
-        });
-    }
-
-    const dbUser: IUser | null = await User.findOne({
-      username
-    });
-
-    if (!dbUser) {
-      Logger.warn(`Error in POST /login: User ${username} not found`);
-
-      return response
-        .status(StatusCodes.NOT_FOUND)
-        .json({
-          message: ReasonPhrases.NOT_FOUND,
-          error: 'User not found'
-        });
-    }
-
-    const isPasswordValid: boolean = await dbUser.comparePassword(password);
-
-    if (!isPasswordValid) {
-      Logger.warn(`Error in POST /login: Invalid password for user ${username}`);
-
-      return response
-        .status(StatusCodes.UNAUTHORIZED)
-        .json({
-          message: ReasonPhrases.UNAUTHORIZED,
-          error: 'Invalid password'
-        });
-    }
-
-    const token: string = JsonWebToken.sign({
-      id: dbUser._id
-    });
-
-    Logger.info(`Success in POST /login: User ${username} logged in`);
-
-    return response
-      .status(StatusCodes.OK)
-      .json({
-        message: ReasonPhrases.OK,
-        token
+      const dbUser: IUser | null = await User.findOne({
+        username
       });
+
+      if (!dbUser) {
+        Logger.warn(`Error in POST /login: User ${username} not found`);
+
+        return response
+          .status(StatusCodes.NOT_FOUND)
+          .json({
+            message: ReasonPhrases.NOT_FOUND,
+            error: 'User not found'
+          });
+      }
+
+      const isPasswordValid: boolean = await dbUser.comparePassword(password);
+
+      if (!isPasswordValid) {
+        Logger.warn(`Error in POST /login: Invalid password for user ${username}`);
+
+        return response
+          .status(StatusCodes.UNAUTHORIZED)
+          .json({
+            message: ReasonPhrases.UNAUTHORIZED,
+            error: 'Invalid password'
+          });
+      }
+
+      const token: string = JsonWebToken.sign({
+        id: dbUser._id
+      });
+
+      Logger.info(`Success in POST /login: User ${username} logged in`);
+
+      return response
+        .status(StatusCodes.OK)
+        .json({
+          message: ReasonPhrases.OK,
+          token
+        });
+    } catch (error) {
+      Logger.error(`Error in POST /login: ${error}`);
+
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+          error
+        });
+    }
   }
 );
 
