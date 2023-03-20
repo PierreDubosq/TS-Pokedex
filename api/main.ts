f99851f34d2ac1ce9645ea5bd85b1a8e52874290
Pierre Dubosq <pierre@dubosq.eu>
2023-03-20 16:41:59 -0600

    Feat: add Seed running

--- api/main.ts
+++ api/main.ts
@@ -1,5 +1,6 @@
 import Database from './sources/Database';
 import Server from './sources/Server';
+import Seed from './utils/Seed';
 
 import { JwtPayload } from 'jsonwebtoken';
 import Logger from 'poseidon-logger';
@@ -14,6 +15,7 @@ declare module 'express' {
 
 async function main(): Promise<void> {
   await Database.connect();
+  await Seed.run();
   await (new Server()).start();
 }
 
