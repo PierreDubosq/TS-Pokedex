672dd366d3bec729118f2d1d67dae77a0c69df62
Pierre Dubosq <pierre@dubosq.eu>
2023-03-20 16:32:33 -0600

    Feat: add run Seed method

--- api/utils/Seed.ts
+++ api/utils/Seed.ts
@@ -1,4 +1,37 @@
+import User from '../models/User';
+import Config from './Config';
+
+import Logger from 'poseidon-logger';
+
+
 class Seed {
+  /**
+   * @brief Seed the database
+   */
+  public static async run(): Promise<void> {
+    try {
+      await User.findOneAndDelete({
+        $or: [
+          {
+            username: Config.ADMIN_USERNAME
+          },
+          {
+            email: Config.ADMIN_EMAIL
+          }
+        ]
+      });
+
+      await User.create({
+        username: Config.ADMIN_USERNAME,
+        password: Config.ADMIN_PASSWORD,
+        email: Config.ADMIN_EMAIL
+      });
+
+      Logger.info('Database seeded');
+    } catch (error) {
+      Logger.error(`Error in Seed.run: ${error}`);
+    }
+  }
 }
 
 
