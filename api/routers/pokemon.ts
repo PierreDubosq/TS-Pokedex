6bc5cb2ef4e2f7dd8ee8329392996b1bc0557c41
Pierre Dubosq <pierre@dubosq.eu>
2023-03-20 17:31:02 -0600

    Feat: add POST /pokemon route

--- api/routers/pokemon.ts
+++ api/routers/pokemon.ts
@@ -3,24 +3,126 @@ import Pokemon from '../models/Pokemon';
 import JsonWebToken from '../utils/JsonWebToken';
 
 import { Request, Response, Router } from 'express';
+import * as fs from 'fs';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import multer from 'multer';
 import Logger from 'poseidon-logger';
 
 
 const router = Router();
+const upload = multer({ dest: 'uploads/' });
 
 
 router.post('/pokemon',
+  upload.single('image'),
   async (
     request: Request,
     response: Response
   ): Promise<Response> => {
     try {
+      const {
+        number,
+        name,
+        types
+      }: {
+        number: number | undefined,
+        name: string | undefined,
+        types: string[] | undefined
+      } = request.body;
+
+      if (!number) {
+        Logger.warn('Error in POST /pokemon: Number is required');
+
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            message: ReasonPhrases.BAD_REQUEST,
+            error: 'Number is required'
+          });
+      }
+
+      if (!name) {
+        Logger.warn('Error in POST /pokemon: Name is required');
+
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            message: ReasonPhrases.BAD_REQUEST,
+            error: 'Name is required'
+          });
+      }
+
+      if (!types) {
+        Logger.warn('Error in POST /pokemon: Types is required');
+
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            message: ReasonPhrases.BAD_REQUEST,
+            error: 'Types is required'
+          });
+      }
+
+      if (!request.file) {
+        Logger.warn('Error in POST /pokemon: Image is required');
+
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            message: ReasonPhrases.BAD_REQUEST,
+            error: 'Image is required'
+          });
+      }
+
+      if (!request.file.mimetype.startsWith('image/')) {
+        Logger.warn('Error in POST /pokemon: Image must be an image');
+
+        return response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({
+            message: ReasonPhrases.BAD_REQUEST,
+            error: 'Image must be an image'
+          });
+      }
+
+      const dbPokemon: IPokemon | null = await Pokemon.findOne({
+        $or: [
+          {
+            number
+          },
+          {
+            name
+          }
+        ]
+      });
+
+      if (dbPokemon) {
+        Logger.warn(`Error in POST /pokemon: Pokemon ${name} already exists`);
+
+        return response
+          .status(StatusCodes.CONFLICT)
+          .json({
+            message: ReasonPhrases.CONFLICT,
+            error: 'Pokemon already exists'
+          });
+      }
+
+      const newPath = `${request.file.destination}${JsonWebToken.sign({ name })}${request.file.originalname.substring(request.file.originalname.lastIndexOf('.'))}`;
+
+      fs.renameSync(request.file.path, newPath);
+
+      const newPokemon: IPokemon = await Pokemon.create({
+        number,
+        name,
+        types,
+        image: newPath
+      });
+
       return response
-        .status(StatusCodes.OK)
+        .status(StatusCodes.CREATED)
         .json({
-          message: ReasonPhrases.OK,
-          data: 'Hello World'
+          message: ReasonPhrases.CREATED,
+          pokemon: newPokemon
         });
     } catch (error) {
       Logger.error(`Error in POST /pokemon: ${error}`);
