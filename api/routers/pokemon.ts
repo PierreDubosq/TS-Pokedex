cdc50d1abb29c68c5a08a758f04b6d3ec75004c6
Pierre Dubosq <pierre@dubosq.eu>
2023-03-20 17:20:25 -0600

    Feat: add POST /pokemon route

--- api/routers/pokemon.ts
+++ api/routers/pokemon.ts
@@ -1,7 +1,39 @@
-import { Router } from 'express';
+import IPokemon from '../interfaces/IPokemon';
+import Pokemon from '../models/Pokemon';
+import JsonWebToken from '../utils/JsonWebToken';
+
+import { Request, Response, Router } from 'express';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import Logger from 'poseidon-logger';
 
 
 const router = Router();
+
+
+router.post('/pokemon',
+  async (
+    request: Request,
+    response: Response
+  ): Promise<Response> => {
+    try {
+      return response
+        .status(StatusCodes.OK)
+        .json({
+          message: ReasonPhrases.OK,
+          data: 'Hello World'
+        });
+    } catch (error) {
+      Logger.error(`Error in POST /pokemon: ${error}`);
+
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({
+          message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+          error: error
+        });
+    }
+  }
+);
 
 
 export default router;
