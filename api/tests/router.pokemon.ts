41c98a8fb4ffd774e2da24d2f45287c4c4b9901c
Pierre Dubosq <pierre@dubosq.eu>
2023-03-21 10:22:01 -0600

    Feat: add /pokemon router unit tests

--- api/tests/router.pokemon.ts
+++ api/tests/router.pokemon.ts
@@ -3,6 +3,7 @@ import { StatusCodes } from 'http-status-codes';
 
 import Server from '../sources/Server';
 import Database from '../sources/Database';
+import Config from '../utils/Config';
 import Seed from '../utils/Seed';
 
 describe('Router /pokemon', () => {
@@ -19,9 +20,36 @@ describe('Router /pokemon', () => {
     await Database.disconnect();
   });
 
-  it('Should return 400', async () => {
+  it('Should return 201', async () => {
+    const accountResponse = await request(server.app).post('/login').send({
+      username: Config.ADMIN_USERNAME,
+      password: Config.ADMIN_PASSWORD,
+    });
+
+    const { token } = accountResponse.body;
+
     const response = await request(server.app)
       .post('/pokemon')
+      .set('Authorization', `${token}`)
+      .attach('file', '/app/tests/assets/bulbasaur.png')
+      .field('number', 1)
+      .field('name', 'Bulbasaur')
+      .field('types', ['Grass', 'Poison']);
+
+    expect(response.status).toBe(StatusCodes.CREATED);
+  });
+
+  it('Should return 400', async () => {
+    const accountResponse = await request(server.app).post('/login').send({
+      username: Config.ADMIN_USERNAME,
+      password: Config.ADMIN_PASSWORD,
+    });
+
+    const { token } = accountResponse.body;
+
+    const response = await request(server.app)
+      .post('/pokemon')
+      .set('Authorization', `${token}`)
       .attach('file', '/app/tests/assets/bulbasaur.png')
       .field('name', 'Bulbasaur')
       .field('types', ['Grass', 'Poison']);
@@ -30,8 +58,16 @@ describe('Router /pokemon', () => {
   });
 
   it('Should return 400', async () => {
+    const accountResponse = await request(server.app).post('/login').send({
+      username: Config.ADMIN_USERNAME,
+      password: Config.ADMIN_PASSWORD,
+    });
+
+    const { token } = accountResponse.body;
+
     const response = await request(server.app)
       .post('/pokemon')
+      .set('Authorization', `${token}`)
       .attach('file', '/app/tests/assets/bulbasaur.png')
       .field('number', 1)
       .field('types', ['Grass', 'Poison']);
@@ -40,8 +76,16 @@ describe('Router /pokemon', () => {
   });
 
   it('Should return 400', async () => {
+    const accountResponse = await request(server.app).post('/login').send({
+      username: Config.ADMIN_USERNAME,
+      password: Config.ADMIN_PASSWORD,
+    });
+
+    const { token } = accountResponse.body;
+
     const response = await request(server.app)
       .post('/pokemon')
+      .set('Authorization', `${token}`)
       .attach('file', '/app/tests/assets/bulbasaur.png')
       .field('number', 1)
       .field('name', 'Bulbasaur');
@@ -50,12 +94,70 @@ describe('Router /pokemon', () => {
   });
 
   it('Should return 400', async () => {
+    const accountResponse = await request(server.app).post('/login').send({
+      username: Config.ADMIN_USERNAME,
+      password: Config.ADMIN_PASSWORD,
+    });
+
+    const { token } = accountResponse.body;
+
     const response = await request(server.app)
       .post('/pokemon')
+      .set('Authorization', `${token}`)
       .field('number', 1)
       .field('name', 'Bulbasaur')
       .field('types', ['Grass', 'Poison']);
 
     expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+  });
+
+  it('Should return 401', async () => {
+    const response = await request(server.app)
+      .post('/pokemon')
+      .attach('file', '/app/tests/assets/bulbasaur.png')
+      .field('number', 1)
+      .field('name', 'Bulbasaur')
+      .field('types', ['Grass', 'Poison']);
+
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+  });
+
+  it('Should return 401', async () => {
+    const response = await request(server.app)
+      .post('/pokemon')
+      .set('Authorization', 'Bad token')
+      .attach('file', '/app/tests/assets/bulbasaur.png')
+      .field('number', 1)
+      .field('name', 'Bulbasaur')
+      .field('types', ['Grass', 'Poison']);
+
+    expect(response.status).toBe(StatusCodes.UNAUTHORIZED);
+  });
+
+  it('Should return 409', async () => {
+    const accountResponse = await request(server.app).post('/login').send({
+      username: Config.ADMIN_USERNAME,
+      password: Config.ADMIN_PASSWORD,
+    });
+
+    const { token } = accountResponse.body;
+
+    await request(server.app)
+      .post('/pokemon')
+      .set('Authorization', `${token}`)
+      .attach('file', '/app/tests/assets/ivysaur.png')
+      .field('number', 2)
+      .field('name', 'Ivysaur')
+      .field('types', ['Grass', 'Poison']);
+
+    const response = await request(server.app)
+      .post('/pokemon')
+      .set('Authorization', `${token}`)
+      .attach('file', '/app/tests/assets/ivysaur.png')
+      .field('number', 2)
+      .field('name', 'Ivysaur')
+      .field('types', ['Grass', 'Poison']);
+
+    expect(response.status).toBe(StatusCodes.CONFLICT);
   });
 });
