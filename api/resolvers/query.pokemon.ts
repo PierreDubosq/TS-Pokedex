e95ce481b3afcfc14ce44950759cc80a85143205
Pierre Dubosq <pierre@dubosq.eu>
2023-03-20 19:09:21 -0600

    Feat: add pokemon query

---
+++ api/resolvers/query.pokemon.ts
@@ -0,0 +1,16 @@
+import IPokemon from '../interfaces/IPokemon';
+import Pokemon from '../models/Pokemon';
+
+
+const pokemon = async (parent: any, args: any): Promise<IPokemon> => {
+  const pokemon: IPokemon | null = await Pokemon.findOne({ number: args.number });
+
+  if (!pokemon) {
+    throw new Error('Pokemon not found');
+  }
+
+  return pokemon;
+}
+
+
+export default pokemon;
